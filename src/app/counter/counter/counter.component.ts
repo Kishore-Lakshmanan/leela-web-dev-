f71770683fb473beea1ae0fe24f5f5995def75e8
Kishore Lakshmanan <kishore14593@gmail.com>
2025-02-08 13:12:24 +0530

    counter using input and output

---
+++ src/app/counter/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+counter:number=0
+constructor(){}
+onInit(){}
+onincrement(){
+this.counter++
+}
+
+ondecrement(){
+  this.counter--
+}
+
+onreset(){
+  this.counter=0
+}
+}
