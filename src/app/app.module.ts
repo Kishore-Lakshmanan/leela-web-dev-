70a4ad4c0fd49932e89f8d15c8330812762e911e
Kishore Lakshmanan <kishore14593@gmail.com>
2025-02-09 14:41:51 +0530

    Post list with routing and added top nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { counterReducer } from './counter/State/counter.reducer';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +22,14 @@ import { CustomCounterInputComponent } from './counter/custom-counter-input/cust
     CounterOutputComponent,
     CounterButtonsComponent,
     CustomCounterInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostsListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }), // Enable only in dev mode
   ],
